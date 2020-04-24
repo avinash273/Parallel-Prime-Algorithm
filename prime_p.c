f6e1bb5dd348e2c24b29a49d4038030a413aedf3
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-24 01:19:16 -0500

    Submission ready both parallel and sequential Sieve algorithm for prime

--- prime_p.c
+++ prime_p.c
@@ -7,7 +7,6 @@ ID: 1001668570
 
 Implemented on Stampede2 (Texas Advanced Computing Center)
 
-
 •	To compile: mpicc prime_p.c
 •	Run the code in 2 modes:
     Mode1(Prime count and Time): ibrun -np 2 ./a.out 1000000
@@ -21,110 +20,111 @@ Implemented on Stampede2 (Texas Advanced Computing Center)
 #include <string.h>
 #include <omp.h>
 
-#define first_prime 2
 #define MASTER_START_INT 1
-int debug = 0;
+#define start_prime 2
 
-void distributeWork(int*, int*, unsigned long*,
+int print_prime = 0;
+
+void Distribute_Work(int*, int*, unsigned long*,
                     unsigned int*, unsigned int*, unsigned int*,
                     unsigned int*, unsigned int*, unsigned int*);
 
 int main(int argc, char** argv) {
 
-   unsigned long n = 4294967295;
-   double timer;
-   int p_count;
-   int p_id;
+   double program_timer;
+   int processor_count;
+   int processor_ID;
    char processor_name[MPI_MAX_PROCESSOR_NAME];
    int name_len;
+   unsigned long n = 4294967295;
 
-   unsigned int n_master;
-   unsigned int n_worker;
+   unsigned int master_node;
+   unsigned int slave_node;
    unsigned int remainder;
-   unsigned int p_work;
-   unsigned int p_first;
-   unsigned int p_last;
+   unsigned int processor_work;
+   unsigned int processor_first;
+   unsigned int processor_last;
    unsigned int prime;
    int j;
    int final_count;
-   char *mark_table;
+   char *mark_index;
 
     MPI_Init(&argc, &argv);
 
     MPI_Barrier(MPI_COMM_WORLD);
-    timer = -MPI_Wtime();
+    program_timer = -MPI_Wtime();
 
-    MPI_Comm_size(MPI_COMM_WORLD, &p_count);
-    MPI_Comm_rank(MPI_COMM_WORLD, &p_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &processor_count);
+    MPI_Comm_rank(MPI_COMM_WORLD, &processor_ID);
 
     if(argc >= 2)
        n = strtoul(argv[1], NULL, 0);
     if(argc == 3 && !strcmp(argv[2],"PRIMES"))
     {
-       debug = 1;
+       print_prime = 1;
     }
 
-    distributeWork(&p_id, &p_count,
+    Distribute_Work(&processor_ID, &processor_count,
                    &n,
-                   &p_last, &p_first, &p_work,
-                   &n_master, &n_worker, &remainder);
+                   &processor_last, &processor_first, &processor_work,
+                   &master_node, &slave_node, &remainder);
 
-    if(debug)
+    if(print_prime)
     {
-       printf("Proc %d: Assigned %d (%d - %d)\n", p_id, p_work, p_first, p_last);
+       printf("Proc %d: Assigned %d (%d - %d)\n", processor_ID, processor_work, processor_first, processor_last);
        MPI_Barrier(MPI_COMM_WORLD);
     }
 
 
-    mark_table = (char*) malloc( sizeof(char)*p_work );
-    if(mark_table == NULL)
+    mark_index = (char*) malloc( sizeof(char)*processor_work );
+    if(mark_index == NULL)
     {
-       printf("Failed to allocate memory for process %d\n", p_id);
+       printf("Failed to allocate memory for process %d\n", processor_ID);
        MPI_Finalize();
        exit(1);
     }
 
-    for(j = 0; j < p_work; j++)
-       mark_table[j] = '0';
+    for(j = 0; j < processor_work; j++)
+       mark_index[j] = '0';
 
-    if(!p_id) mark_table[0] = '1';
+    if(!processor_ID) mark_index[0] = '1';
 
     int marker;
-    prime = first_prime;
+    prime = start_prime;
     do
     {
 
-       if(prime < p_first)
+       if(prime < processor_first)
        {
-          int mod = p_first % prime;
-          if(mod)
-             marker = prime - mod;
+          int check_reminder = processor_first % prime;
+          if(check_reminder)
+             marker = prime - check_reminder;
 
           else
              marker = 0;
        }
        else
        {
-          marker = 2*prime - p_first;
+          marker = 2*prime - processor_first;
        }
 
 
-       for(j = marker; j < p_work; j += prime)
+       for(j = marker; j < processor_work; j += prime)
        {
-          mark_table[j] = '1';
+          mark_index[j] = '1';
        }
 
 
-       if(!p_id)
+       if(!processor_ID)
        {
           int next_index = prime - MASTER_START_INT;
           do{
-             if(++next_index >= n_master)
+             if(++next_index >= master_node)
              {
-                next_index == n_master - MASTER_START_INT;
+                next_index == master_node - MASTER_START_INT;
                 break;
              }
-          }while(mark_table[next_index] != '0');
+          }while(mark_index[next_index] != '0');
           prime = next_index + MASTER_START_INT;
        }
 
@@ -132,31 +132,31 @@ int main(int argc, char** argv) {
        MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 
-    }while(prime <= n_master );
+    }while(prime <= master_node );
 
-    if(debug)
+    if(print_prime)
     {
 
-       if(p_id)
+       if(processor_ID)
        {
-          MPI_Recv(&prime, 1, MPI_INT, (p_id-1), 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          MPI_Recv(&prime, 1, MPI_INT, (processor_ID-1), 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
        }
 
 
-       printf("Proc %d: ", p_id);
-       for(j = 0; j < p_work; j++)
+       printf("Processor %d: ", processor_ID);
+       for(j = 0; j < processor_work; j++)
        {
-          if(mark_table[j] == '0')
-             printf("%d, ", j + p_first);
+          if(mark_index[j] == '0')
+             printf("%d, ", j + processor_first);
        }
        printf("\n");
        fflush(stdout);
 
 
-       if(p_id != (p_count - 1))
+       if(processor_ID != (processor_count - 1))
        {
 
-          MPI_Send(&prime, 1, MPI_INT, (p_id+1), 1, MPI_COMM_WORLD);
+          MPI_Send(&prime, 1, MPI_INT, (processor_ID+1), 1, MPI_COMM_WORLD);
 
        }
     }
@@ -165,22 +165,22 @@ int main(int argc, char** argv) {
     prime = 0;
 
     #pragma omp parallel for reduction(+:prime)
-    for(j = 0; j < p_work; j++)
+    for(j = 0; j < processor_work; j++)
     {
-       if(mark_table[j] == '0')
+       if(mark_index[j] == '0')
           prime++;
     }
     MPI_Reduce(&prime, &final_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
 
     MPI_Barrier(MPI_COMM_WORLD);
-    timer += MPI_Wtime();
+    program_timer += MPI_Wtime();
 
 
-    if(!p_id)
+    if(!processor_ID)
     {
-     if(debug) printf("Count of primes up to %d: %d\n", n, final_count);
-     printf("N:%lu,P:%d,Time:%10.6f\n",n,p_count,timer);
+     if(print_prime) printf("Get Primes till %d: %d\n", n, final_count);
+     printf("Number:%lu Processors:%d  Time consumed:%10.6f\n",n,processor_count,program_timer);
     }
 
 
@@ -189,45 +189,45 @@ int main(int argc, char** argv) {
 
 
 
-void distributeWork(int *p_id,
-                    int *p_count,
+void Distribute_Work(int *processor_ID,
+                    int *processor_count,
                     unsigned long *n,
-                    unsigned int *p_last,
-                    unsigned int *p_first,
-                    unsigned int *p_work,
-                    unsigned int *n_master,
-                    unsigned int *n_worker,
+                    unsigned int *processor_last,
+                    unsigned int *processor_first,
+                    unsigned int *processor_work,
+                    unsigned int *master_node,
+                    unsigned int *slave_node,
                     unsigned int *remainder)
 {
-    *n_master = (unsigned int) ceil(sqrt((double) *n));
-    *n_worker = (*n - *n_master)/(*p_count - 1);
-    *remainder = (*n - *n_master)%(*p_count - 1);
+    *master_node = (unsigned int) ceil(sqrt((double) *n));
+    *slave_node = (*n - *master_node)/(*processor_count - 1);
+    *remainder = (*n - *master_node)%(*processor_count - 1);
 
 
-    if(*n_worker > *n_master)
+    if(*slave_node > *master_node)
     {
-       *n_master = *n_worker = *n / *p_count;
-       *remainder = *n % *p_count;
+       *master_node = *slave_node = *n / *processor_count;
+       *remainder = *n % *processor_count;
     }
 
-    if(!*p_id)
+    if(!*processor_ID)
     {
-       *p_last = *p_work = *n_master;
-       *p_first = MASTER_START_INT;
+       *processor_last = *processor_work = *master_node;
+       *processor_first = MASTER_START_INT;
     }
     else
     {
-       *p_work = *n_worker;
+       *processor_work = *slave_node;
 
-       if(*p_id <= *remainder)
-          (*p_work)++;
+       if(*processor_ID <= *remainder)
+          (*processor_work)++;
 
-       *p_last = *n_master + (*n_worker * *p_id);
+       *processor_last = *master_node + (*slave_node * *processor_ID);
 
-       if(*p_id <= *remainder)
-          *p_last += *p_id;
+       if(*processor_ID <= *remainder)
+          *processor_last += *processor_ID;
        else
-          *p_last += *remainder;
-       *p_first = *p_last - *p_work + 1;
+          *processor_last += *remainder;
+       *processor_first = *processor_last - *processor_work + 1;
     }
 }
